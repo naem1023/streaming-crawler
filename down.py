dd3cb71bb60c0beafd6b751eef16d38e867791c3
naem1023 <relilau00@gmail.com>
2020-01-01 16:29:32 +0900

    change method to aria2

--- down.py
+++ down.py
@@ -1,6 +1,7 @@
 # python3
 import urllib.request as request
 import os
+import traceback
 
 def createFolder(directory):
     try:
@@ -17,6 +18,7 @@ def readUrlTxt():
 
 	return urlList
 
+
 def getVideoName():
 	videoName = open("name.txt", 'r')
 	name = videoName.readline()
@@ -25,50 +27,100 @@ def getVideoName():
 def downTsFile(videoName, urlList):
 	fakeReferer = 'https://avgle.com'
 
-	for i in range(len(urlList)):
-		#request with fake header
-		videoReq = request.Request(url=urlList[i], headers={'User-Agent': 'Mozilla/5.0', 'referer' : fakeReferer})
+	videoFolderExist = False
+	len_fileListTxt = 0
+	fileListTxtName = os.path.join(videoName, "fileList.txt")
 
-		#get video
-		videoPiece = request.urlopen(videoReq).read()
 
-	
+	try:
+		command = """aria2c -c -x 4 --header="Referer: """ + fakeReferer + """" -d """ + str(videoName) + " -i tslist.txt"
+		os.system(command)
 
+	except Exception as error:
+		print('-' * 20)
+		traceback.print_exc()
+
+	'''
+	#check existence of downloaded files
+	if os.path.isfile(fileListTxtName) :
+		#text file : already saved m38u file
+		
+		fileListTxtFile = open(fileListTxtName, 'r')
+
+		fileListTxt = fileListTxtFile.readlines()
+		len_fileListTxt = len(fileListTxt)
+
+		videoFolderExist = True
+
+	for i, v in enumerate(urlList):
 		fileName = str(i).zfill(4) + ".m3u8"
 		filePath = os.path.join(videoName, fileName)
+
+		#check existence of file
+		if videoFolderExist:
+			if i <= len_fileListTxt - 1 :
+				fileListTxtFile.close()
+				continue;
+
+		print("start request " + fileName)
+		
+		#request with fake header
+		videoReq = request.Request(url=v, headers={'User-Agent': 'Mozilla/5.0', 'referer' : fakeReferer})
+
+		#get video
+		#videoPiece = request.urlopen(videoReq).read()
 
 		createFolder(videoName)
 
 		#save video
-		with open(filePath, "wb") as file:
-			file.write(videoPiece)
+		#with open(filePath, "wb") as file:
+		#	file.write(videoPiece)
+		
 
 		print("file save : " + fileName)
 
 		#write file path with filename
 		#using this file in FFMPEG
-		temp = "file " + "'" +  filePath + "'\n"
-		with open(os.path.join(videoName, "fileList.txt"), 'a') as file:
-			file.write(temp)
+		#temp = "file " + "'" +  filePath + "'\n"
+		#with open(fileListTxtName, 'a') as file:
+		#	file.write(temp)
+		'''
 
 
 def mergeFile(videoName):
-	command = "ffmpeg -f concat -safe 0 -i fileList.txt -c copy "+ videoName + ".mp4"
+	command = "ffmpeg -f concat -safe 0 -i "+ str(videoName) + "\\fileList.txt -c copy "+ videoName + ".mp4"
 	os.system(command)
 
+def makeTsList(videoName):
+	fileListTxtFile = open(os.path.join(videoName, "fileList.txt"), 'w')
+
+	#get file list of video folder
+	fileList = os.listdir(videoName)
+
+	#find ts file from list
+	for file in fileList:
+		ext = os.path.splitext(file)[-1]
+		if ext == '.ts':
+			temp = "file '" + videoName + '\\' + str(file) + "'\n"
+			fileListTxtFile.write(temp)
+
+	fileListTxtFile.close()
+
 def main():
-	try:
+	#try:
 		urlList = readUrlTxt()
 
 		videoName = getVideoName()
 
 		downTsFile(videoName, urlList)
 
+		makeTsList(videoName)
+
 		mergeFile(videoName)
 
-	except Exception as error:
-		print('-' * 20)
-		print('error occured', error)
+	#except Exception as error:
+	#	print('-' * 20)
+	#	traceback.print_exc()
 
 if __name__ == "__main__":
 	main()
